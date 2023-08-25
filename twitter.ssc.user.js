26f09a6c8e110037c3595fb67a98c0eca8508886
eccos <eccos@users.noreply.github.com>
2023-08-25 00:35:00 -0700

    add dowload and update urls

--- twitter.ssc.user.js
+++ twitter.ssc.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Show sensitive content
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Auto clicks "Show" button for "Content warning: Sensitive content" overlay
 // @author       eccos
 // @match        https://twitter.com/*
-// @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
+// @icon         https://www.google.com/s2/favicons?domain=twitter.com
 // @grant        none
+// @downloadURL  https://github.com/eccos/Twitter-Show-sensitive-content/raw/main/twitter.ssc.user.js
+// @updateURL    https://github.com/eccos/Twitter-Show-sensitive-content/raw/main/twitter.ssc.user.js
 // ==/UserScript==
 
 (function() {
