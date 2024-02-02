d0899e3cccbce0fe5d5255ca6cefff79dfc08d24
eccos <eccos@users.noreply.github.com>
2024-02-02 11:02:45 -0800

    hide blocked posts

--- twitter.ssc.user.js
+++ twitter.ssc.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Show sensitive content
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Auto clicks "Show" button for "Content warning: Sensitive content" overlay
 // @author       eccos
 // @match        https://twitter.com/*
+// @match        https://x.com/*
 // @icon         https://www.google.com/s2/favicons?domain=twitter.com
 // @grant        none
 // @downloadURL  https://github.com/eccos/Twitter-Show-sensitive-content/raw/main/twitter.ssc.user.js
@@ -12,22 +13,37 @@
 // ==/UserScript==
 
 (function () {
-    'use strict';
+  'use strict';
 
-    function autoClickShow() {
-        let btns = document.querySelectorAll("div[role='button']");
-        btns.forEach((btn) => {
-            if (btn.textContent != "Show") return;
-            btn.click();
-        });
-    }
-    let ticking = false;
-    document.addEventListener('scroll', (e) => {
-        if (ticking) return;
-        window.requestAnimationFrame(() => {
-            autoClickShow();
-            ticking = false;
-        });
-        ticking = true;
+  function autoClickShow() {
+    const btns = document.querySelectorAll("div[role='button']");
+    btns.forEach((btn) => {
+      if (btn.textContent != 'Show') return;
+      btn.click();
     });
+  }
+
+  function hideBlockedPosts() {
+    const blockStr = 'This Post is from an account you blocked.';
+    const posts = document.querySelectorAll("article[role='article']");
+    posts.forEach((post) => {
+      const spans = post.querySelectorAll('span');
+      spans.forEach((span) => {
+        if (span.textContent.includes(blockStr)) {
+          post.style.display = 'none';
+        }
+      });
+    });
+  }
+
+  let ticking = false;
+  document.addEventListener('scroll', (e) => {
+    if (ticking) return;
+    window.requestAnimationFrame(() => {
+      autoClickShow();
+      hideBlockedPosts();
+      ticking = false;
+    });
+    ticking = true;
+  });
 })();
