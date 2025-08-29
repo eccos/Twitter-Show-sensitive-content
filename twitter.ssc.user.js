1c26f25c0ea450d2a4f467af3e6a3005f443e549
eccos <eccos@users.noreply.github.com>
2025-08-29 13:42:43 -0700

    refactor scroll handler & autoclick sensitive profile

--- twitter.ssc.user.js
+++ twitter.ssc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Show sensitive content
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Auto clicks "Show" & "View" buttons for "Sensitive content" overlays
 // @author       eccos
 // @match        https://twitter.com/*
@@ -10,16 +10,26 @@
 // @grant        none
 // @downloadURL  https://github.com/eccos/Twitter-Show-sensitive-content/raw/main/twitter.ssc.user.js
 // @updateURL    https://github.com/eccos/Twitter-Show-sensitive-content/raw/main/twitter.ssc.user.js
+// @require      file://C:/Users/Eccos/Dev/repos/Twitter-Show-sensitive-content/twitter.ssc.user.js
 // ==/UserScript==
 
 (function () {
   'use strict';
 
+  document.addEventListener('scroll', handleScroll);
+
+  function handleScroll(e) {
+    window.requestAnimationFrame(() => {
+      autoClickShow();
+      hideBlockedPosts();
+    });
+  }
+
   function autoClickShow() {
     const oldBtns = document.querySelectorAll("div[role='button']"); // twitter method of defining the button
     const newBtns = document.querySelectorAll("button[role='button']"); // x method of defining the button
     const btns = [...oldBtns, ...newBtns];
-    const txtMatchList = ['show', 'view'];
+    const txtMatchList = ['show', 'view', 'yes, view profile'];
 
     btns.forEach((btn) => {
       const txt = btn.textContent.toLowerCase();
@@ -40,15 +50,4 @@
       });
     });
   }
-
-  let ticking = false;
-  document.addEventListener('scroll', (e) => {
-    if (ticking) return;
-    window.requestAnimationFrame(() => {
-      autoClickShow();
-      hideBlockedPosts();
-      ticking = false;
-    });
-    ticking = true;
-  });
 })();
